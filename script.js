0991aa766ab8572521a15b3e719c5d96883920cb
Douglas Ede <70340545+Douglas4739@users.noreply.github.com>
2020-09-19 15:03:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ let pizza = ["Pepperoni & Jalapeno", "Meatlovers", "Steak & Bacon"];
 
 function getPizza(per1){
   for(let i = 0; i < per1.length; i++)
-  console.log(pizza[i])
+  console.log(pizza[i]);
 }
 //getPizza(pizza);
 
 function randomPizza(per1){
+  for (let i =0; i < per1.length; i++)
   Math.floor(Math.random() * 3);
+ if(i = 1 == true){
+   console.log(pizza[0]);
+ }
+ if(i = 2 == true){
+   console.log(pizza[1]);
+ }
+ if(i = 3 ==true){
+   console.log(pizza[2]);
+ }
 }
 
 getPizza(randomPizza)
+randomPizza(pizza)
